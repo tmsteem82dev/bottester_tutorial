00ed60da6e6653e8e9d3d143fe0af61854fa1bd5
TimS <tmdev82@gmail.com>
2018-08-01 20:33:04 +0200

    finished basic tests

---
+++ bot.js
@@ -0,0 +1,18 @@
+const builder = require("botbuilder");
+module.exports = function (connector) {
+    let bot = new builder.UniversalBot(connector)
+
+    bot.dialog("/", function (session) {
+        switch (session.message.text.toLocaleLowerCase()) {
+            case "hiya":
+                session.send("Good day");
+                break;
+            default:
+                session.send("I don't understand: " + session.message.text);
+                break;
+        }
+
+    });
+
+    return bot;
+};
---
+++ test/basic.dialog.test.js
@@ -0,0 +1,32 @@
+const {
+    BotTester,
+    TestConnector
+} = require("bot-tester");
+const MyBot = require('../bot');
+const connector = new TestConnector();
+
+describe("BotTester", () => {
+    let bot;
+    beforeEach(() => {
+        bot = require("../bot")(connector);
+    });
+
+    it("can reply hello", function () {
+
+        return new BotTester(bot).sendMessageToBot("Hiya", "Good day")
+            .runTest();
+    });
+
+    let messages = ["Who are you?", "How are you doing?", "x"]
+    for (let i = 0; i < messages.length; i++) {
+
+        it("will reply with I do not understand", function () {
+            let msg = messages[i];
+            return new BotTester(bot).sendMessageToBot(msg, "I don't understand: " + msg)
+                .runTest();
+        });
+    }
+
+
+
+});
