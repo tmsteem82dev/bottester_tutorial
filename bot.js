ea15a97ec97d32e0f4ce3692e063990d751cc785
TimS <tmdev82@gmail.com>
2018-08-02 20:11:11 +0200

    multiple replies

--- bot.js
+++ bot.js
@@ -5,13 +5,12 @@ module.exports = function (connector) {
     bot.dialog("/", function (session) {
         switch (session.message.text.toLocaleLowerCase()) {
             case "hiya":
-                session.send("Good day");
+                session.send(["Good day", "Hello", "Hi", "Hemoglobin"]);
                 break;
             default:
                 session.send("I don't understand: " + session.message.text);
                 break;
         }
-
     });
 
     return bot;
--- test/basic.dialog.test.js
+++ test/basic.dialog.test.js
@@ -11,11 +11,13 @@ describe("BotTester", () => {
         bot = require("../bot")(connector);
     });
 
-    it("can reply hello", function () {
+    for (let i = 0; i < 5; i++) {
+        it("can reply hello", function () {
 
-        return new BotTester(bot).sendMessageToBot("Hiya", "Good day")
-            .runTest();
-    });
+            return new BotTester(bot).sendMessageToBot("Hiya", ["Good day", "Hello", "Hi"])
+                .runTest();
+        });
+    }
 
     let messages = ["Who are you?", "How are you doing?", "x"]
     for (let i = 0; i < messages.length; i++) {
